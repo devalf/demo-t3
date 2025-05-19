3c45bd92befbe70c8992b539acd849851835c9e3
Oleksiy Petrychko <mykolasich@gmail.com>
2025-05-19 15:26:59 +0300

    Development of Auth Service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,19 +1,31 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Demo-t3 | Auth service - Swagger API')
+    .setDescription('API documentation for demo-t3 auth service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+  SwaggerModule.setup('docs', app, document); // open swagger docs at the /docs endpoint
+
+  const port = process.env.NX_PUBLIC_AUTH_SERVICE_PORT || 8084;
+
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
