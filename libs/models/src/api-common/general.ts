54ebeb589fcc13e83f83a4f52b34e62643c8f19f
Oleksiy Petrychko <mykolasich@gmail.com>
2025-09-30 15:51:36 +0300

    Configs adjustments

--- libs/models/src/api-common/general.ts
+++ libs/models/src/api-common/general.ts
@@ -8,4 +8,4 @@ export type ApiListMeta = {
 
 export type ProductCondition = 'new' | 'used' | 'refurbished';
 
-export type Dummy = string; // need this line to trigger `affected changes` detection
+export type Dummy = number; // need this line to trigger `affected changes` detection
